1da3f2cf8658db9399c71ed061a19385a63b0bd6
kor1212v <kor1212v@gmailcom>
2021-06-15 13:20:19 +0900

    추가 파일

--- ch04/js/event.js
+++ ch04/js/event.js
@@ -1,12 +1,18 @@
 
 function showText(){
-   var text = document.getElementById('desc');
-   text.style.display = "block";
+    //var text = document.getElementById('desc');
+   // text.style.display = "block";
+    document.getElementById('desc').style.display = "block" ;
 
+    //var button = document.getElementById('open');
+    //button.style.display = "none";
   
+    document.getElementById('open').style.display = "none";
 }
 function hideText(){
     var text = document.getElementById('desc');
     text.style.display = "none";
    
+    var button = document.getElementById('open');
+    button.style.display = "block";
 }
